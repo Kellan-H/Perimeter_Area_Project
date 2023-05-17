b6a426bc25b3b7a147dfdb46ba439a1a4650671c
Kellan-H <124210369+Kellan-H@users.noreply.github.com>
2023-05-18 10:40:43 +1200

    Add files via upload

--- 00_main.py
+++ 00_main.py
@@ -10,18 +10,23 @@ tool = 0
 #----------------- Functions -----------------
 def select_shape(event):
     if clicked.get() == 'Square':
+        square_input()
         shape = "Square"
         print(shape)
     if clicked.get() == 'Triangle':
+        del_square_input()
         shape = "Triangle"
         print(shape)
     if clicked.get() == 'Circle':
+        del_square_input()
         shape = "Circle"
         print(shape)
     if clicked.get() == 'Rectangle':
+        del_square_input()
         shape = "Rectangle"
         print(shape)
     if clicked.get() == 'Paralellogram':
+        del_square_input()
         shape = "Paralellogram"
         print(shape)
 
@@ -33,6 +38,20 @@ def select_tool():
     else:
         tool = "area"
         print(tool)
+
+#---------------  INPUT FUNCTIONS  -------------------
+def square_input():
+    global mylabel
+    global a
+    mylabel = Label(text="Width/Length:")
+    mylabel.pack()
+    a = Entry(root, width=10)
+    a.pack()
+
+def del_square_input():
+    mylabel.destroy()
+    a.destroy()
+
 
 options = [
     "Square",
